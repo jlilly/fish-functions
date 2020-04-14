3e87886c20bc8d5c18b21bd9fa8ef584204d33ed
Joe <joe.a.lilly@gmail.com>
2020-04-14 10:57:50 -0600

    install-git-lg

---
+++ install-git-lg.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function install-git-lg
+	git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+end
